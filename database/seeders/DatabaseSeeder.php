5cb4797522d619866c3082fb0ffc224c8b72ce0a
sagarkhunt <sagarkhunt3@gmail.com>
2023-06-21 11:02:57 +0530

    some changes in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Hash;
+use Illuminate\Support\Facades\Hash as FacadesHash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,7 +21,7 @@ class DatabaseSeeder extends Seeder
         $user->name = "Sagar Khunt";
         $user->phone_number = "9725516774";
         $user->email = "sagar@admin.com";
-        $user->password = Hash::make('admin@123');
+        $user->password = FacadesHash::make('admin@123');
         $user->role_id = '1';
         // $user->user_type = 'admin';
         $user->save();
